27d5ff8b37dec3c9f316386f7fd0260c2705cc27
mikrozar <mikrozar@mail.ru>
2021-10-17 23:54:27 +0300

    Тестовый комментарий

---
+++ src/Loader.java
@@ -0,0 +1,11 @@
+public class Loader
+
+    public class Scanner {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите первый операнд:");
+            int operand1 = scanner.nextInt();
+            System.out.println(operand1);
+        }
+    }
+}
---
+++ src/Scaner.java
@@ -0,0 +1,12 @@
+public class Scaner {
+    import java.util.Scanner;
+
+    public class Scaner1 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите первый операнд:");
+            int operand1 = scanner.nextInt();
+            System.out.println(operand1);
+        }
+    }
+}
